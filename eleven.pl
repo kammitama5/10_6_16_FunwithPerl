ebe924180c26b6af09d683a12b38f51a1bd02b86
kammitama5 <krystal.maughan@gmail.com>
2016-10-06 10:40:32 -0700

    1st commit

---
+++ eleven.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+#concat scalars
+
+$str = "Hi" . " Krystal! ";
+$num = 55 + 20;
+$mul = 55 * 2;
+$mix = $str . $num;
+
+print "str: $str\n";
+print "num: $num\n";
+print "mul: $mul\n";
+print "mix: $mix\n";
