689114a525319367cc5e7a1426a55a2ba4bc6dc4
kammitama5 <krystal.maughan@gmail.com>
2016-10-06 09:31:21 -0700

    1st commit

---
+++ seven.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+#arrays
+
+@favourite_foods = ("Bananas", "IceCream", "apples", "carrots");
+@people = ("John", "Penny", "Helen", "Marc");
+@scores = (54, 98, 76, 44);
+
+print "A favourite food is $favourite_foods[1].\n";
+print "I know this lady named $people[2].\n";
+print "She scored $scores[3] in her last exam.\n";
