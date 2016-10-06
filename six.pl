f41e5927051c8b361f0eff12c0b49271f19b4c31
kammitama5 <krystal.maughan@gmail.com>
2016-10-06 09:23:11 -0700

    1st commit

---
+++ six.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+#using scalars in Perl
+
+$name = "Bob";
+$salary = 20000;
+$location = "Illinois";
+
+print "The gentleman I met is $name, he lives in $location and makes $salary a year\n";
