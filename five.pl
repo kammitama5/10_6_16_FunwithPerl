c3f051b88b8102a6efe4499b322f0dcad0d862f9
kammitama5 <krystal.maughan@gmail.com>
2016-10-06 09:17:55 -0700

    1st commit

---
+++ five.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+#string manipulation
+
+$str = "\uhello Krystal\n";
+print "$str";
+#prints "Hello Krystal"
+
+$str = "\Uhello Krystal\n";
+print "$str";
+#prints "HELLO KRYSTAL"$str = "hel\Ulo Kryst\E.al \n";
+print "$str";
+#prints "helLO KRYSTal"
+
