cdcc08dcb62fc8380011492adb6995e9523dab25
kammitama5 <krystal.maughan@gmail.com>
2016-10-06 10:35:21 -0700

    1st commit
    scalars

---
+++ ten.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+#is everything in Perl a Scalar?
+
+$integer = 500;
+$negative = -1000;
+$floating = 500.42;
+$bigfloat = -1.2E-54;
+
+$octal = 0377;
+$hexa = 0xff;
+
+print "integer = $integer\n";
+print "negative = $negative\n";
+print "floating = $floating\n";
+print "bigfloat = $bigfloat\n";
+print "octal = $octal\n";
+print "hexa = $hexa\n";
