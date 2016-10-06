968c377033488bd275cf11effe86a32d1cfd187e
kammitama5 <krystal.maughan@gmail.com>
2016-10-06 09:44:59 -0700

    1st commit

---
+++ eight.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+#key value pairs
+
+%database = ('Samantha Harris', 22, 'Tiffany LeBlanc', 63, 'John Brown', 34);
+
+print "\$data for Samantha Harris :  {'Samantha Harris'} = $database{'Samantha Harris'}\n";
+print "\$data for Tiffany LeBlanc :  {'Tiffany LeBlanc'} = $database{'Tiffany LeBlanc'}\n";
+print "\$data for John Brown :  {'John Brown'} = $database{'John Brown'}\n"; 
