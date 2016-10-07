9d825f5347dfe5df382ebecec7226fc63ad5428a
kammitama5 <krystal.maughan@gmail.com>
2016-10-07 12:53:11 -0700

    2nd commit

--- fiftythree.pl
+++ fiftythree.pl
@@ -9,4 +9,8 @@ $string =~ tr/a-z/b/d;
 $string = 'food';
 $string =~ tr/a-z/a-z/s;
 
+$string1 = 'foodjljwler3433oooooarljlere';
+$string1 = 'foodjljwler3433oooooarljlere';
+$string1 =~ tr/a-z/a-z/s;
 print "$string\n";
+print "$string1\n";
