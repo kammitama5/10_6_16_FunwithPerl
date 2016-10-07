4374bd0c63ec40516b44670caffabf6eee928724
kammitama5 <krystal.maughan@gmail.com>
2016-10-07 11:15:34 -0700

    2nd commit

---
+++ fifty.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+%ages = ("Michael Caine", 39,
+	"Dirty Den", 34,
+	"Angie", 27,
+	"Willy", "21 in dog years",
+	"The Queen Mother", 108);
+	
+@p1 = $ages{"Michael Caine"};
+@p2 = $ages{"Dirty Den"};
+@p3 = $ages{"Angie"};
+@p4 = $ages{"Willy"};
+@p5 = $ages{"The Queen Mother"};
+	
+print("Age Michael Caine: @p1\n");
+print("Age Dirty Den: @p2\n");
+print("Age Angie: @p3\n");
+print("Age Willy: @p4\n");
+print("Age The Queen Mother: @p5\n");
