eece7c09ebac1d70afba03f42aa6b1f97d051fd5
kammitama5 <krystal.maughan@gmail.com>
2016-10-07 12:32:58 -0700

    2nd commit

--- fiftytwo.pl
+++ fiftytwo.pl
@@ -6,5 +6,11 @@ $string =~ s/hot/dirty/;
 $string1 = "The hot cat sat under the house.";
 $string1 =~ s/hot/big/;
 
+$string2 = "The hot cat sat under the hot house.";
+$string2 =~ tr/h/i/;
+
 print "$string\n";
 print "$string1\n";
+print "$string2\n";
+
+
