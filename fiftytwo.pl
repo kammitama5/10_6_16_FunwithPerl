a4fe8ca2a005b86ee5832adda007ed8c0bba6220
kammitama5 <krystal.maughan@gmail.com>
2016-10-07 12:31:02 -0700

    2nd commit

--- fiftytwo.pl
+++ fiftytwo.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl
 
-$string = "The hot cat sat under the hot house";
+$string = "The hot cat sat under the hot house.";
 $string =~ s/hot/dirty/;
 
+$string1 = "The hot cat sat under the house.";
+$string1 =~ s/hot/big/;
+
 print "$string\n";
+print "$string1\n";
